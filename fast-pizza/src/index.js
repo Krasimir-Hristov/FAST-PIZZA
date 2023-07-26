a2c6dfa3dc3b705e2732ec46785412a2ddb93eaf
krasi <krasimir.xristov@gmail.com>
2023-07-26 20:21:20 +0200

    Using React.fragment

--- fast-pizza/src/index.js
+++ fast-pizza/src/index.js
@@ -4,91 +4,98 @@ import './index.css';
 
 
 const pizzaData = [
-    {
-        name: 'Focaccia',
-        ingredients: 'Bread with italian olive oil and rosemary',
-        price: 6,
-        photoName: 'pizzas/focaccia.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Margherita',
-        ingredients: 'Tomato and mozarella',
-        price: 10,
-        photoName: 'pizzas/margherita.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Spinaci',
-        ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-        price: 12,
-        photoName: 'pizzas/spinaci.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Funghi',
-        ingredients: 'Tomato, mozarella, mushrooms, and onion',
-        price: 12,
-        photoName: 'pizzas/funghi.jpg',
-        soldOut: false,
-    },
-    {
-        name: 'Pizza Salamino',
-        ingredients: 'Tomato, mozarella, and pepperoni',
-        price: 15,
-        photoName: 'pizzas/salamino.jpg',
-        soldOut: true,
-    },
-    {
-        name: 'Pizza Prosciutto',
-        ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-        price: 18,
-        photoName: 'pizzas/prosciutto.jpg',
-        soldOut: false,
-    },
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
 ];
 
 
 function App() {
-    return <div className='container'>
-        <Header />
-        <Menu />
-        <Footer />
-    </div>;
+  return <div className='container'>
+    <Header />
+    <Menu />
+    <Footer />
+  </div>;
 }
 
 function Header() {
 
-    // const style = { color: 'red', fontSize: '48px', textTransform: 'uppercase' };
-    const style = {};
+  // const style = { color: 'red', fontSize: '48px', textTransform: 'uppercase' };
+  const style = {};
 
-    return (
-        <header className='header footer'>
-            <h1 style={style}  >Fast Pizza Co.</h1>
-        </header>
-    );
+  return (
+    <header className='header'>
+      <h1 style={style}  >Fast Pizza Co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
-    const pizzas = pizzaData;
-    // const pizzas = [];
-    const numPizzas = pizzas.length;
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
 
-    return (
-        <main className='menu'>
-            <h2>Our Menu</h2>
-
-            {numPizzas > 0 ? (
-                <ul className='pizzas'>
-                    {pizzas.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
-            ) : <p>We're still working on our menu. Please come back later. :)</p>}
+  return (
+    <main className='menu'>
+      <h2>Our Menu</h2>
 
 
 
-            {/* <Pizza
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <p>
+            Bread with italian olive oil and rosemary Tomato, mozarella, spinach, and ricotta cheese
+          </p>
+          <ul className='pizzas'>
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>) :
+        <p>We're still working on our menu. Please come back later. :)</p>}
+
+
+
+      {/* <Pizza
                 name='Pizza Spinaci'
                 ingredients='Tomato, mozarella, ham, aragula, and burrata cheese'
                 photoName='pizzas/spinaci.jpg'
@@ -101,65 +108,65 @@ function Menu() {
                 price={12}
                 photoName='pizzas/funghi.jpg'
             /> */}
-        </main>
-    );
+    </main>
+  );
 }
 
 function Pizza({ pizzaObj }) {
 
-    console.log(pizzaObj);
+  console.log(pizzaObj);
 
-    if (pizzaObj.soldOut) {
-        return null;
-    };
+  if (pizzaObj.soldOut) {
+    return null;
+  };
 
-    return (
-        <li className='pizza'>
-            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-            <div>
-                <h3>{pizzaObj.name}</h3>
-                <p>{pizzaObj.ingredients}</p>
-                <span>{pizzaObj.price}</span>
-            </div>
-        </li>
-    );
+  return (
+    <li className='pizza'>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}</span>
+      </div>
+    </li>
+  );
 }
 
 function Footer() {
 
-    const hour = new Date().getHours();
-    const openHour = 12;
-    const closeHour = 22;
-    const isOpen = hour >= openHour && hour <= closeHour;
-    console.log(isOpen);
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
 
-    return (
-        <footer className='footer'>
-            {isOpen ? (
-                <Order closeHour={closeHour} openHour={openHour}
-                />) :
-                <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>
-            }
-        </footer >
-    );
+  return (
+    <footer className='footer'>
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour}
+        />) :
+        <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00</p>
+      }
+    </footer >
+  );
 }
 
 
 function Order({ closeHour, openHour }) {
 
-    return (
-        <div className='order'>
-            <p>
-                We're open from {openHour}:00 until {closeHour}:00. Come visit us or order online
-            </p>
-            <button className='btn'>Order</button>
-        </div>);
+  return (
+    <div className='order'>
+      <p>
+        We're open from {openHour}:00 until {closeHour}:00. Come visit us or order online
+      </p>
+      <button className='btn'>Order</button>
+    </div>);
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
